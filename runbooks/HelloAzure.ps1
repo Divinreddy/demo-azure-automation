d067b21368a1c07e21351915ae6fc4e32fb292bf
Mike Wood <mvwood@mvwood.com>
2016-08-25 10:00:29 -0400

    Created HelloAzure.ps1 in AzureAutomation.

---
+++ runbooks/HelloAzure.ps1
@@ -0,0 +1,23 @@
+Param
+(
+    [Parameter (Mandatory= $false)]
+    [string] $uriToTest = "http://azure.com"
+)
+#Adapted from: https://gallery.technet.microsoft.com/scriptcenter/Powershell-Script-for-13a551b3
+try{ 
+    $requestTime = Measure-Command { $testRequest = Invoke-WebRequest -Uri $uriToTest -UseBasicParsing } 
+    $result = [PSCustomObject] @{ 
+      Time = Get-Date; 
+      Uri = $uri; 
+      StatusCode = [int] $testRequest.StatusCode; 
+      StatusDescription = $testRequest.StatusDescription; 
+      ResponseLength = $testRequest.RawContentLength; 
+      TimeTakenInMilliseconds =  $requestTime.TotalMilliseconds;  
+      } 
+    Write-Output $result
+  }  
+  catch 
+  { 
+    Write-Error "Check failed with '$_.Exception'"
+  }   
+  
